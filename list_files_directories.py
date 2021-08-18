6f764d1f627c9091ef59a2050ccc6582c03163da
Farshid PirahanSiah, PhD <pirahansiah@gmail.com>
2021-08-18 14:11:54 +0200

    list of specific files in specific folder in python
    list of specific files in specific folder in python

---
+++ list_files_directories.py
@@ -0,0 +1,19 @@
+###################################################################################### import 
+import os
+import glob
+###################################################################################### config
+root="C:\\farshid\\"
+specific_directories=root+"/**/farshid/**/*.jpg"
+path_dir_detection_check=""
+
+###################################################################################### function 
+files= glob.glob(specific_directories, recursive=True)
+for file in files:     
+    b=file.rfind("farshid")
+    path_dir_detection=file[0:b-1]    
+    if (path_dir_detection != path_dir_detection_check):        
+        dirname = os.path.dirname(file)
+        print("******************************************************* Next Directories ************************")
+        print(dirname)    
+        path_dir_detection_check=path_dir_detection       
+    print(file) 
